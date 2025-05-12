926b6b0f64d376b70450e30387db0835f5239164
Ilya <Ilia.Mikhailov@mcon-group.com>
2025-05-13 00:15:49 +0200

    refactor: remove unused panZoom import and usage from main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import panZoom from 'vue-panzoom' // @ts-ignore
 
 const app = createApp(App)
-app.use(panZoom)
 app.mount('#app')
